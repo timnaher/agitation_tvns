1233831492dd533e720c6148ba77a402e3ce7179
timnaher <tim.naeher@googlemail.com>
2024-09-22 20:43:45 +0200

    updated scripts and implemented socket triggers

--- hackathon/collect_data_for_training.py
+++ hackathon/collect_data_for_training.py
@@ -1,17 +1,21 @@
-
 import psychopy.visual
 import psychopy.event
 import time
 import random
 import numpy as np
+import socket
+
+# Initialize socket
+socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+endPoint = ("127.0.0.1", 1000)
 
 # Global variables
-win = None # Global variable for window (Initialized in main)
-fixation = None # Global variable for fixation cross (Initialized in main)
+win = None  # Global variable for window (Initialized in main)
+fixation = None  # Global variable for fixation cross (Initialized in main)
 bg_color = [0, 0, 0]
 win_w = 800
 win_h = 600
-refresh_rate = 165.0 # Monitor refresh rate (CRITICAL FOR TIMING)
+refresh_rate = 165.0  # Monitor refresh rate (CRITICAL FOR TIMING)
 
 #========================================================
 # High Level Functions
@@ -19,40 +23,48 @@ refresh_rate = 165.0 # Monitor refresh rate (CRITICAL FOR TIMING)
 def Paradigm(trials):
     # Initialize fixation cross
     fixation = psychopy.visual.ShapeStim(
-                win=win,
-                units='pix',
-                size=50,
-                fillColor=[1, 1, 1],
-                lineColor=[1, 1, 1],
-                lineWidth=1,
-                vertices='cross',
-                name='off',
-                pos=[0, 0]
-            )
+        win=win,
+        units='pix',
+        size=50,
+        fillColor=[1, 1, 1],
+        lineColor=[1, 1, 1],
+        lineWidth=1,
+        vertices='cross',
+        name='off',
+        pos=[0, 0]
+    )
     
     # Initialize text (for instructions & results)
     text = psychopy.visual.TextStim(win,
-                    'TEST TEXT', font='Open Sans', units='pix', 
-                    pos=(0, 0), alignText='center',
-                    height=36, color=[1, 1, 1]
-                    )
+                                    'TEST TEXT', font='Open Sans', units='pix',
+                                    pos=(0, 0), alignText='center',
+                                    height=36, color=[1, 1, 1]
+                                    )
 
     for trial in trials:
-        # 1000 ms direction instruction (blink or no blink)
+        # Send trigger for the start of the trial (trigger 100)
+        socket.sendto(b"100", endPoint)
+        
+        # 1000 ms direction instruction (normal or hyperventilation)
         text.text = trial
+        
+        # Send trigger and get the trigger value for the condition
+        trigger = trigger_dict[trial]
+        socket.sendto(b"%d" % trigger, endPoint)
+
         for frame in range(MsToFrames(1500, refresh_rate)):
             text.draw()
             win.flip()
-            
+        
         # 500ms blank screen
         for frame in range(MsToFrames(500, refresh_rate)):
             win.flip()
-    
-        # 3000ms fixation cross (doing MI)
+
+        # 5000ms fixation cross (doing condition - hyperventilation or rest)
         for frame in range(MsToFrames(5000, refresh_rate)):
             fixation.draw()
             win.flip()
-            
+
         # 500ms blank screen
         for frame in range(MsToFrames(500, refresh_rate)):
             win.flip()
@@ -60,12 +72,17 @@ def Paradigm(trials):
         # 1000ms blank screen
         for frame in range(MsToFrames(1000, refresh_rate)):
             win.flip()
-    
+
+        # Send trigger for the end of the trial (trigger 200)
+        socket.sendto(b"200", endPoint)
+
     print("Paradigm complete")
+
 
 def MsToFrames(ms, fs):
     dt = 1000 / fs
     return np.round(ms / dt).astype(int)
+
 
 if __name__ == "__main__":
     # Set random seed
@@ -84,8 +101,10 @@ if __name__ == "__main__":
     # Wait a second for the window to settle down
     time.sleep(1)
 
-    # Define the trials: 10 "blink" and 10 "no blink"
+    # Define the trials: 10 "normal" and 10 "hyperventilation"
     trials = ['normal', 'hyperventilation'] * 10
+    # Generate a trigger dictionary for conditions
+    trigger_dict = {'normal': 1, 'hyperventilation': 2}
     random.shuffle(trials)
 
     # Run through paradigm
