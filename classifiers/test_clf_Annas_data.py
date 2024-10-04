357d40b91dfd69c9966b550143bde893d357c139
vorreuth <anna.vorreuther@iao.fraunhofer.de>
2024-10-04 18:39:01 +0200

    clf on Annas data (with loo implementation commented)

--- classifiers/test_clf_Annas_data.py
+++ classifiers/test_clf_Annas_data.py
@@ -1,7 +1,13 @@
-import pandas as pd
-import joblib
 import os
+
+import joblib
 import numpy as np
+import pandas as pd
+from pyriemann.classification import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.model_selection import LeaveOneOut
+
 from agitation_tvns.src.model_pipelines import BlockKernels
 
 Fs = 250  # Sampling frequency
@@ -29,18 +35,20 @@ data.columns = ['EEG1', 'EEG2', 'EEG3', 'EEG4', 'EEG5', 'EEG6', 'EEG7', 'EEG8', 
 start_indices = data.index[data['Trigger'] == trigger_start].tolist()[:-1]
 end_indices = data.index[data['Trigger'] == trigger_end].tolist()
 
+trigger_indices = data.index[data['Trigger'].isin([1.0, 2.0])].tolist()
 
 # cut first 2 sec of data
-data = data.iloc[2*Fs:]
+data = data.iloc[2 * Fs:]
 
 # epoch the data
 epochs = []
 labels = []
 for start, end in zip(start_indices, end_indices):
+    # for start in trigger_indices:
     # Extract data for each trial
     epoch = data.iloc[start:(start + duration)]
-    # get labels per epoch
     markers = set(epoch["Trigger"])
+    # get labels per epoch
     if 1.0 in markers:
         label = "rest"
     elif 2.0 in markers:
@@ -48,10 +56,12 @@ for start, end in zip(start_indices, end_indices):
     else:
         label = None
     if label:
+        trigger = epoch.index[epoch['Trigger'].isin([1.0, 2.0])].tolist()[0]
+        epoch = data.iloc[trigger:(trigger + duration)]
         epoch = epoch.drop(columns=["Trigger"])
         epoch = [epoch.iloc[i:i + 2 * Fs] for i in range(0, len(epoch), 2 * Fs)]
         epochs.append(epoch)
-        labels.append([label]*len(epoch))
+        labels.append([label] * len(epoch))
 
 labels = [x for xs in labels for x in xs]
 conditions = set(labels)
@@ -62,9 +72,8 @@ print("found conditions: {conditions} in data".format(conditions=conditions))
 
 # generate X and y from epochs
 X = np.array([np.array(epoch).T for epoch in epochs])
-X = X.reshape(X.shape[0]*X.shape[-1], 14, 500)
+X = X.reshape(X.shape[0] * X.shape[-1], 14, 500)
 y = np.array(labels)
-
 
 # Print the shape of the resulting arrays
 print("trials (X) shape:", X.shape)  # Expected shape: (n_trials, 250, n_channels)
@@ -73,8 +82,46 @@ print("labels (y) shape:", y.shape)  # Expected shape: (n_trials,)
 X_tim = np.load(tim_data_path)
 assert X_tim.shape[1:] == X.shape[1:]
 
-y_pred = model.predict(X)
 
-# calculate accuracy
-accuracy = np.mean(y_pred == y)
-print(f'accuracy:{accuracy}')
+kernel_functions = ["linear", "poly", "polynomial", "rbf", "laplacian", "cosine"]
+
+
+# Leave-One-Out Cross-Validation
+# loo = LeaveOneOut()
+# accuracies = []
+# for train_index, test_index in loo.split(X):
+#     # Split data
+#     X_train, X_test = X[train_index], X[test_index]
+#     y_train, y_test = y[train_index], y[test_index]
+#
+#     model = Pipeline(
+#     [("block_kernels", BlockKernels(
+#         block_size=[8, 3, 3], metric=["rbf", "corr", 'rbf'], shrinkage=[0.1, 0.1, 0.1], ),), ("classifier", SVC()), ]
+#     )
+#     model.fit(X_train, y_train)
+#
+#     # Predict on test data
+#     y_pred = model.predict(X_test)
+#
+#     # Calculate accuracy for this iteration
+#     accuracy = np.mean(y_pred == y_test)
+#     accuracies.append(accuracy)
+#
+# # calculate accuracy
+# mean_accuracy = np.mean(accuracies)
+# print(f'loo accuracy:{mean_accuracy}')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, shuffle=True)
+model = Pipeline(
+    [("block_kernels", BlockKernels(
+        block_size=[8, 3, 3], metric=["rbf", "corr", 'rbf'], shrinkage=[0.1, 0.1, 0.1], ),), ("classifier", SVC()), ]
+    )
+model.fit(X_train, y_train)
+
+# Predict on test data
+y_pred = model.predict(X_test)
+
+# Calculate accuracy for this iteration
+accuracy = np.mean(y_pred == y_test)
+
+print(f'train-test-split accuracy:{accuracy}')
